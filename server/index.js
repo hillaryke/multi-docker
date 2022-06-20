6815c9f7d1d74060ad4f655de0be618b45ad745e
hillaryme <tenskap@gmail.com>
2022-06-20 17:35:57 +0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const keys = require('./keys');
+
+// Express App Setup
+const express = reqiure('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.user(bodyParser.json());
+
+// Postgres Client Setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+   user: keys.pgUser,
+   host: keys.pgHost,
+   database: keys.pgDatabase,
+   password: keys.pgPassword,
+   port: keys.pgPort
+});
+pgClient.on('error', () => console.log('Lost PG connection'));
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+   .catch(err => console.log(err));
