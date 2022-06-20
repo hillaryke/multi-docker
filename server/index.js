cc2cf7abb6ac4b04fe4b09a9dc4bfa12bc5866c3
hillaryme <tenskap@gmail.com>
2022-06-20 17:51:25 +0300

    More Express API Setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const keys = require('./keys');
 
 // Express App Setup
-const express = reqiure('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -22,3 +22,54 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
    .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.CreateClient({
+   host: keys.redisHost,
+   port: keys.rediPort,
+   retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+
+app.get('/', ( req, res ) => {
+   res.send('Hello There');
+});
+
+// Retrieve all values or indices submitted to postgres
+app.get('/values/all', async ( req, res ) => {
+   const values = await pgClient.query('SELECT * from values');
+
+   res.send(values.rows);
+});
+
+// Reach to Redis and return values for all indices
+app.get('/values/current', async ( req, res ) => {
+   redisClient.hgetall('values', ( err, values ) => {
+      res.send(values);
+   });
+});
+
+app.post('/values', async ( req, res ) => {
+   const index = req.body.index;
+
+   if (parseInt(index) > 40) {
+      return res.status(422).send('Index too high');
+   }
+
+   // put index value as 'Nothing yet' to redis store
+   redisClient.hset('values', index, 'Nothing yet!');
+
+   // tell redis to start calculating fib value for the index
+   redisPublisher.publish('insert', index);
+   pgClient.query('INSERT INTO(number) VALUES($1)', [index]);
+
+   res.send({ working: true });
+});
+
+app.listen(5000, err => {
+   console.log('Listening');
+});
