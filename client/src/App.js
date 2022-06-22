7d418146678744044135425d335a6dbdb4e520f3
hillaryme <tenskap@gmail.com>
2022-06-22 18:00:27 +0300

    Replace package.json for matching version

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 function App() {
    return (
-      <div className="App">
-         <Router>
+      <Router>
+         <div className="App">
             <header className="App-header">
-               <img src={logo} className="App-logo" alt="logo"/>
+               <img src={logo} className="App-logo" alt="logo" />
                <a
                   className="App-link"
                   href="https://reactjs.org"
@@ -19,16 +19,15 @@ function App() {
                >
                   Learn React 3
                </a>
-               <Link to="/">Home </Link>
+               <Link to="/">Home</Link>
                <Link to="/otherpage">Other Page</Link>
             </header>
-            <Switch>
-               <Route exact path="/" component={Fib}/>
+            <div>
+               <Route exact path="/" component={Fib} />
                <Route path="/otherpage" component={OtherPage} />
-            </Switch>
-         </Router>
-      </div>
-
+            </div>
+         </div>
+      </Router>
    );
 }
 
