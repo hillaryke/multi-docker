fbb545a003e2b4821446c89672aed13b5b3a369c
hillaryme <tenskap@gmail.com>
2022-06-22 17:47:26 +0300

    Use Switch in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
 function App() {
    return (
-      <Router>
-         <div className="App">
+      <div className="App">
+         <Router>
             <header className="App-header">
-               <img src={logo} className="App-logo" alt="logo" />
+               <img src={logo} className="App-logo" alt="logo"/>
                <a
                   className="App-link"
                   href="https://reactjs.org"
@@ -22,12 +22,13 @@ function App() {
                <Link to="/">Home </Link>
                <Link to="/otherpage">Other Page</Link>
             </header>
-            <Routes>
-               <Route exact path="/" component={Fib} />
+            <Switch>
+               <Route exact path="/" component={Fib}/>
                <Route path="/otherpage" component={OtherPage} />
-            </Routes>
-         </div>
-      </Router>
+            </Switch>
+         </Router>
+      </div>
+
    );
 }
 
