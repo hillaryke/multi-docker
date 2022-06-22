8347808cca497a4d3c2bbf8a7c2020dd696da696
hillaryme <tenskap@gmail.com>
2022-06-22 22:20:26 +0300

    Replaced index.js file with grider's

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const pgClient = new Pool({
    host: keys.pgHost,
    database: keys.pgDatabase,
    password: keys.pgPassword,
-   port: keys.pgPort
+   port: keys.pgPort,
 });
 
 pgClient.on('connect', () => {
@@ -29,49 +29,43 @@ pgClient.on('connect', () => {
 const redis = require('redis');
 const redisClient = redis.createClient({
    host: keys.redisHost,
-   port: keys.rediPort,
-   retry_strategy: () => 1000
+   port: keys.redisPort,
+   retry_strategy: () => 1000,
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
-app.get('/', ( req, res ) => {
-   res.send('Hello There');
+app.get('/', (req, res) => {
+   res.send('Hi');
 });
 
-// Retrieve all values or indices submitted to postgres
-app.get('/values/all', async ( req, res ) => {
+app.get('/values/all', async (req, res) => {
    const values = await pgClient.query('SELECT * from values');
 
    res.send(values.rows);
 });
 
-// Reach to Redis and return values for all indices
-app.get('/values/current', async ( req, res ) => {
-   redisClient.hgetall('values', ( err, values ) => {
+app.get('/values/current', async (req, res) => {
+   redisClient.hgetall('values', (err, values) => {
       res.send(values);
    });
 });
 
-app.post('/values', async ( req, res ) => {
+app.post('/values', async (req, res) => {
    const index = req.body.index;
 
    if (parseInt(index) > 40) {
       return res.status(422).send('Index too high');
    }
 
-   // put index value as 'Nothing yet' to redis store
    redisClient.hset('values', index, 'Nothing yet!');
-
-   // tell redis to start calculating fib value for the index
    redisPublisher.publish('insert', index);
    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
    res.send({ working: true });
 });
 
-app.listen(5000, err => {
+app.listen(5000, (err) => {
    console.log('Listening');
 });
