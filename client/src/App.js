899913ceb98c6a6152b6281719a915eac56146ea
hillaryme <tenskap@gmail.com>
2022-06-22 17:18:52 +0300

    Verify code

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
                >
                   Learn React 3
                </a>
-               <Link to="/">Home</Link>
+               <Link to="/">Home </Link>
                <Link to="/otherpage">Other Page</Link>
             </header>
             <div>
